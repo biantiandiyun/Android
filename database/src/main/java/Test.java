3056ae26ebcf4766cb1f23c359d4210e95778479
biantiandiyun <819452973@qq.com>
2017-01-02 16:14:27 +0800

    new

--- database/src/main/java/Test.java
+++ database/src/main/java/Test.java
@@ -1,5 +1,46 @@
+import lzhw.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
 /**
  * Created by lzhw on 2016/12/31.
  */
 public class Test {
+
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+
+    static {
+
+        try {
+            reader = Resources.getResourceAsReader("config/mybatis/Configuration.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+//            User user = (User) session.selectOne("userMapper.GetUserByID", 1);
+            System.err.println("now start:"+System.currentTimeMillis());
+            for (int i = 0; i < 100000; i++) {
+                User user = new User();
+                user.setAge(i);
+                user.setName("user"+i);
+                session.insert("userMapper.insert",user);
+                if (i>=100&&i%100==0) {
+                    session.commit();
+                }
+            }
+            System.err.println("end:"+System.currentTimeMillis());
+        } finally {
+            session.close();
+        }
+    }
 }
---
+++ database/src/main/java/lzhw/model/User.java
@@ -0,0 +1,63 @@
+package lzhw.model;
+
+/**
+ * Created by lzhw on 2017/1/1.
+ */
+public class User {
+
+    private int id;
+    private Integer age;
+    private String name;
+    private String dept;
+    private String phone;
+    private String website;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDept() {
+        return dept;
+    }
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
